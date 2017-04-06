e3e1feee2497e1f07ef97eda7d83e4cbbca6b7ec
Klaus Kämpf <kkaempf@suse.de>
2017-04-06 19:38:54 +0200

    Re-use Tabs from Supportconfigs

--- public/jsx/supportconfig.jsx
+++ public/jsx/supportconfig.jsx
@@ -1,9 +1,39 @@
-var SupportConfig = React.createClass({
-  getInitialState: function() {
-    return { name: null };
-  },
-  render: function() {
-    console.log("SupportConfig.render name " + this.props.name);
-    return(<div>Name: {this.props.name}</div>);
+class SupportConfigElement extends React.Component {
+  render() {
+    console.log("SupportConfigElement " + this.props.element);
+    return(<div>{this.props.element}</div>);
   }
-});
+}
+
+class SupportConfig extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: []
+    };
+  }
+  componentDidMount() {
+    $.get("/supportconfig/elements/" + this.props.name).done(function(elements) {
+      this.setState({
+        elements: elements
+      });
+    }.bind(this));
+  }
+  dummy() {
+    this.state.elements.map((element) => {
+      <SupportConfigElement element={element} key={element}/>
+    });
+  }
+  render() {
+    console.log("SupportConfig.render name " + this.props.name);
+    if (this.state.elements.length == 0) {
+      return(<div>Loading {this.props.name} elements ...</div>);
+    }
+    else {
+      console.log("SupportConfig.render " + this.state.elements.length + " elements");
+      return(
+        <Tabs firstName="Supportconfig"/>
+      );
+    }
+  }
+}
