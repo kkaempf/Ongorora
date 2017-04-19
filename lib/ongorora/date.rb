c352c8d3651a41d1277423f53ea12abfd44e9805
Klaus Kämpf <kkaempf@suse.de>
2017-04-19 14:52:57 +0200

    Cope with missing Date

--- lib/ongorora/date.rb
+++ lib/ongorora/date.rb
@@ -6,7 +6,7 @@ module Ongorora
       db = ::Ongorora.database
       res = db.read(:index => 'elasticsupport', :type => 'date', :where => { 'name' => name }).first
       STDERR.puts "Date.get #{name} : #{res.inspect}"
-      Time.at(res['date'].to_i / 1000)
+      Time.at(res['date'].to_i / 1000) rescue nil
     end
   end
 end
